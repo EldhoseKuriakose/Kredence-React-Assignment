7626c34399e780e93f4d87e3773dee5f01fd479a
Eldhose Kuriakose <eldhosekuriakose96@gmail.com>
2020-09-26 14:24:21 +0530

    login-page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import NavTabs from './components/Navbar/Navbar.component';
+import Login from './components/Login/Login.component';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <NavTabs />
+      <Switch>
+        <Route exact path='/' component={NavTabs} />
+        <Route exact path='/login' component={Login} />
+      </Switch>
     </div>
   );
 }
